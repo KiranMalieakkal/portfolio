c1d4c344f2ac5eb347debc14867b36fa5f1bbbbe
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-11-13 15:39:55 +0100

    Adding fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        merriweather: ["Merriweather"],
+        oswald: ["Oswald"],
+        libre: ["Libre Caslon Text", 'serif'],
+        facultyglyphic: ["Faculty Glyphic"]
+      },
+      colors: {
+        custom: {
+          primary: "#740938",
+          secondary: "#AF1740",
+          tertiary: "#CC2B52",
+          quaternary: "#DE7C7D",
+        },
+      },
+    },
   },
   plugins: [
     require('daisyui'),
