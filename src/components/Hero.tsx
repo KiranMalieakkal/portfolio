11bf975cdcc940cbb8181295487ea1949260f3a9
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-11-14 15:21:12 +0100

    Making Hero and TopNav responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,10 +2,12 @@ import image from "../assets/Portofolio_Image-squared.png";
 function Hero() {
   return (
     <>
-      <div className=" flex justify-center items-center min-h-[calc(100vh-123.5px)] ">
+      <div className=" flex justify-center items-center  min-h-[calc(100vh-123.5px)] ">
         <div className="">
-          <div className="-mb-5 px-10">
-            <p className="font-haviland text-[64px]">Hi, I am Kiran!</p>
+          <div className="lg:-mb-5 px-10">
+            <p className="font-haviland lg:text-[64px] md:text-[64px] text-[44px] mt-10 text-center lg:text-start md:text-start">
+              Hi, I am Kiran!
+            </p>
           </div>
           <div className="flex">
             <div className="flex flex-col">
@@ -17,12 +19,12 @@ function Hero() {
               </div>
             </div>
             <div className="flex flex-col max-w-[635px] bg-red-100 leading-[2.5rem] px-[60px] py-[32px]">
-              <p className="text-[40px] text-center font-libre font-light">
+              <p className="lg:text-[40px] text-[25px] text-center font-libre font-light">
                 Crafting seamless digital experiences by blending front-end
                 finesse with back-end resilience is what I'm good at
               </p>
               <div className="w-full flex items-center justify-center p-4">
-                <button className=" bg-slate-900 text-white w-1/5">
+                <button className=" bg-slate-900 text-white w-[100px]">
                   About Me
                 </button>
               </div>
--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -3,7 +3,7 @@ function TopNav() {
     <>
       <nav className="border-t-4 border-custom-primary max-h-[123.5px]">
         <div className="flex flex-row p-4">
-          <div className="flex justify-around items-center grow ">
+          <div className="lg:flex justify-around items-center grow hidden ">
             <div>
               <a className="tracking-widest">HOME</a>
             </div>
@@ -15,13 +15,15 @@ function TopNav() {
             </div>
           </div>
 
-          <div className="flex flex-col grow-0">
-            <p className="font-libre text-[53px] font-extrabold -mb-4 p-0">
+          <div className="flex flex-col lg:grow-0 grow items-center justify-center">
+            <p className="font-libre lg:text-[53px] md:text-[53px] text-[40px] font-extrabold -mb-4 p-0 text-center">
               Kiran Malieakkal
             </p>
-            <p className="font-facultyglyphic font-bold text-center mt-0">FULLSTACK DEVELOPER</p>
+            <p className="font-facultyglyphic font-bold text-center mt-0">
+              FULLSTACK DEVELOPER
+            </p>
           </div>
-          <div className="flex justify-around items-center grow ">
+          <div className="lg:flex justify-around items-center grow hidden ">
             <div>
               <a className="tracking-widest">BLOG</a>
             </div>
